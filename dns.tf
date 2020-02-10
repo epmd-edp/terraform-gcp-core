259ffdf879a34caf02ab151431b1e7811fef3084
Oleksandr Kosse <oleksandr_kosse@epam.com>
2020-02-10 12:35:13 +0100

    Add DNS zone and fixed external IP address
    Change-Id: Iade006ffb25591b79477589d2cf194b0f5c9cdbd

---
+++ dns.tf
@@ -0,0 +1,45 @@
+# Requires roles/dns.admin
+module "dns-public-zone" {
+  source  = "terraform-google-modules/cloud-dns/google"
+  project_id = var.project_id
+  type       = "public"
+  name       = "${var.platform_name}-zone"
+  domain     = var.domain
+
+  recordsets = [
+    {
+      name = "*"
+      type = "A"
+      ttl  = 600
+      records = module.address.addresses
+    },
+    {
+      name = ""
+      type = "NS"
+      ttl  = 21600
+      records = [
+        "ns-cloud-c1.googledomains.com.",
+        "ns-cloud-c2.googledomains.com.",
+        "ns-cloud-c3.googledomains.com.",
+        "ns-cloud-c4.googledomains.com."
+      ]
+    },
+    {
+      name = "localhost"
+      type = "A"
+      ttl  = 300
+      records = [
+        "127.0.0.1",
+      ]
+    },
+    {
+      name = ""
+      type = "TXT"
+      ttl  = 300
+      records = [
+        "\"v=spf1 -all\"",
+      ]
+    },
+  ]
+}
+
