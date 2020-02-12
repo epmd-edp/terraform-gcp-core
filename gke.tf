3292dc3c0a26fcb0efe3202c7b91eef8f0a6f2cb
Oleksandr Kosse <oleksandr_kosse@epam.com>
2020-02-12 17:00:01 +0100

    Add a load balancer
    Change-Id: I4cee6b23358595eea1d4ff72c099ad0981088d1d

--- gke.tf
+++ gke.tf
@@ -1,74 +1,49 @@
-module "gke" {
-  source                     = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
-  project_id                 = var.project_id
-  name                       = var.platform_name
-  region                     = var.region
-  regional                   = false
-  zones                      = var.zones
-  network                    = module.gcp-network.network_name
-  subnetwork                 = module.gcp-network.subnets_names[index(module.gcp-network.subnets_names, local.subnet_name)]
-  ip_range_pods              = local.pods_range_name
-  ip_range_services          = local.svc_range_name
-  kubernetes_version         = "1.15.8-gke.2"
-  http_load_balancing        = false
-  horizontal_pod_autoscaling = false
-  # TODO var
-  network_policy             = false
-  remove_default_node_pool   = true
-  enable_private_endpoint    = false
-  enable_private_nodes       = true
-  master_ipv4_cidr_block     = "172.16.0.0/28"
+resource "google_container_cluster" "primary" {
+  name                  = var.platform_name
+  location              = var.zones[0]
+  initial_node_count    = 1
+  network               = module.gcp-network.network_name
+  subnetwork            = module.gcp-network.subnets_names[index(module.gcp-network.subnets_names, local.subnet_name)]
+  min_master_version        = "1.15.8-gke.3"
+  # TODO discuss more parameters
+  private_cluster_config {
+    enable_private_nodes    = true
+    enable_private_endpoint = false
+    master_ipv4_cidr_block  = "172.16.0.0/28"
+  }
+  ip_allocation_policy {
+    cluster_secondary_range_name  = local.pods_range_name
+    services_secondary_range_name = local.svc_range_name
+  }
+  addons_config {
+    http_load_balancing {
+      disabled = true
+    }
+  }
 
-  master_authorized_networks = [
-    # {
-    #   cidr_block   = data.google_compute_subnetwork.subnetwork.ip_cidr_range
-    #   display_name = "VPC"
-    # },
-  ]
+  master_auth {
+    username = ""
+    password = ""
 
-  node_pools = [
-    {
-      name               = "${var.platform_name}-node-pool"
-      # TODO var
-      machine_type       = "n1-standard-1"
-      autoscaling        = false
-      local_ssd_count    = 0
-      disk_size_gb       = 100
-      disk_type          = "pd-standard"
-      image_type         = "COS"
-      auto_repair        = true
-      auto_upgrade       = true
-      # TODO change
-      preemptible        = true
-      # TODO var
-      node_count         = 1
-    },
-  ]
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
 
-  node_pools_oauth_scopes = {
-    all = [
+  node_config {
+    preemptible  = false
+    oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
     ]
-  }
 
-  node_pools_labels = {
-    all = {}
-
-    default-node-pool = {
-      default-node-pool = true
+    metadata = {
+      disable-legacy-endpoints = "true"
     }
   }
 
-  node_pools_metadata = {
-    all = {}
-
-    edp-cluster-node-pool = {
-      node-pool-metadata-custom-value = "my-node-pool"
-    }
-  }
-
-  node_pools_tags = {
-    all = []
+  timeouts {
+    create = "30m"
+    update = "40m"
   }
 }
---
+++ lb.tf
@@ -0,0 +1,47 @@
+resource "google_compute_global_forwarding_rule" "frontend" {
+  name       = "${var.platform_name}-frontend"
+  target     = google_compute_target_http_proxy.load_balancer.self_link
+  port_range = "80"
+  ip_address = module.address.addresses[0]
+}
+
+resource "google_compute_target_http_proxy" "load_balancer" {
+  name        = "${var.platform_name}-load-balancer"
+  url_map     = google_compute_url_map.url_map.self_link
+}
+
+resource "google_compute_url_map" "url_map" {
+  name            = "${var.platform_name}-url-map"
+  default_service = google_compute_backend_service.lb-backend.self_link
+}
+
+resource "google_compute_health_check" "tcp-health-check" {
+  name = "${var.platform_name}-health-check"
+
+  timeout_sec        = 5
+  check_interval_sec = 5
+
+  tcp_health_check {
+    port = "32767"
+  }
+}
+
+resource "google_compute_backend_service" "lb-backend" {
+  name          = "${var.platform_name}-lb-backend"
+  port_name     = "ng-ingress"
+  health_checks = [google_compute_health_check.tcp-health-check.self_link]
+  backend {
+    # TODO define it
+    group                   = google_container_cluster.primary.instance_group_urls[0]
+  }
+
+}
+
+resource "null_resource" "set_named_port" {
+  provisioner "local-exec" {
+    command = "gcloud compute instance-groups set-named-ports ${google_container_cluster.primary.instance_group_urls[0]} --named-ports=ng-ingress:32767"
+  }
+  depends_on = [
+    google_container_cluster.primary,
+  ]
+}
